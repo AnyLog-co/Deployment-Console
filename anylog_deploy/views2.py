fbc5937a16ac2b4ffb6975a30bb84d654fe2bca8
Moshe <moshe@anylog.co>
2021-12-11 15:09:51 -0800

    deploy code

--- anylog_deploy/views2.py
+++ anylog_deploy/views2.py
@@ -6,18 +6,20 @@ from django.http import HttpResponseRedirect
 
 al_forms = {
 
-    "Deploy_Node" : {
+    "deploy_node" : {
 
         "name" : "Deploy Node",
+        "next" : "set_connections",         # Next page
         "fields" :
         [
             {
-                "name" : "Config File",
+                "key" : "config_file",
+                "label" : "Config File",
                 "type" : "selection",
                 "options" : os.path.join(os.path.dirname(os.path.abspath(__file__)), 'configs')
             },
             {
-                "name": "my_text",
+                "key": "my_text",
                 "label": "My Text",
                 "type": "input_text",
                 "size" : 80,
@@ -25,15 +27,32 @@ al_forms = {
 
             {
                 "break" : 3,
-                "name": "External Config File",
+                "label": "External Config File",
+                "key": "external_config_file",
                 "type": "button_submit",
             },
             {
-                "name": "Deploy Node",
+                "label": "Deploy Node",
+                "key": "deploy_node",
                 "type": "button_submit",
             },
 
         ]
+    },
+
+    "set_connections" : {
+
+        "name" : "Set Connections",
+        "next" : "deploy_node",         # Next page
+        "fields":
+            [
+                {
+                    "label": "Config File",
+                    "key": "config_file",
+                    "type": "selection",
+                    "options": os.path.join(os.path.dirname(os.path.abspath(__file__)), 'configs')
+                },
+            ]
     }
 
 }
@@ -41,4 +60,26 @@ al_forms = {
 
 class Example:
     def front_page(self, request)->HttpResponse:
-        return render(request, 'generic.html', al_forms["Deploy_Node"])
+
+        if request.method == 'POST':
+            next_page_name = update_config(request)
+
+        else:
+            next_page_name = "deploy_node"      # First page to display
+
+        al_forms[next_page_name]["page_name"] = next_page_name      # store the page key to analyze the data
+        return render(request, 'generic.html', al_forms[next_page_name])
+
+
+def update_config(request):
+
+    post_data = request.POST
+    page_name = post_data.get('page_name')
+
+    # get field values
+    form_defs = al_forms[page_name]
+    fields = form_defs["fields"]
+    for field in fields:
+
+
+    pass
